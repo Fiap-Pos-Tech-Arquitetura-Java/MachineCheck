00f51a7fcc5a975e57c3446b80aaf6b51dd6c6b8
anderson.wagner <anderson.wagner@gmail.com>
2023-10-15 19:14:40 -0300

    create project

---
+++ src/main/java/br/com/fiap/postech/machinecheck/MachineCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.machinecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineCheckApplication.class, args);
+    }
+
+}
